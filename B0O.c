6a315c7a6bbce3836aa7b8c78a5143d5614ab543
alex-bellon <alexrbellon@gmail.com>
2019-04-25 16:38:54 -0500

    Initial commit

---
+++ B0O.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define OOOOOO int
+#define OOOOO0 main
+#define OOOO0O (
+#define OOOO00 )
+#define OOO0OO {
+#define OOO0O0 printf
+#define OOO00O "Hell00OO00O0, world!\n"
+#define OOO000 ;
+#define OO0OOO return
+#define OO0OO0 0
+#define OO0O0O }
+
+OOOOOO OOOOO0 OOOO0O OOOO00 OOO0OO
+  OOO0O0 OOOO0O OOO00O OOOO00 OOO000
+  OO0OOO OO0OO0 OOO000
+OO0O0O
